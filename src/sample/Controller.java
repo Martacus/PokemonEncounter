e3b2a31b24ce785db32356922ef01b0b330fc7d4
Martacus <mcpkmart@hotmail.nl>
2016-12-11 19:28:07 +0100

    Timer for seconds no encounter

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,18 +1,32 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class Controller {
+
+    Timer t = new Timer( );
 
     @FXML
     public Label encounter;
 
+    int seconds = 0;
+
+    @FXML
+    public Label secondsLabel;
+
     public void onMouseIncrease(){
         Main.counter++;
         encounter.setText(Main.counter + "");
+
+        seconds = 0;
+        setSecondsText();
     }
 
     public void onMouseDecrease(){
@@ -30,4 +44,24 @@ public class Controller {
     public void show(){
         encounter.setText(Main.counter + "");
     }
+
+    @FXML
+    public void initialize() {
+        t.schedule(new TimerTask() {
+            public void run() {
+                Platform.runLater(new Runnable() {
+                    public void run() {
+                        seconds++;
+                        setSecondsText();
+                    }
+                });
+            }
+        }, 0, 1000);
+    }
+
+    void setSecondsText(){
+        secondsLabel.setText(seconds + "");
+    }
+
+
 }
