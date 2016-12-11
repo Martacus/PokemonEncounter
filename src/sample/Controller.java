3ac3f02c7231ca54ac19ea88519abbfa918bd49d
Martacus <mcpkmart@hotmail.nl>
2016-12-11 19:58:04 +0100

    Added saving of the encounter number

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,8 +3,9 @@ package sample;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
 
+import java.io.*;
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -27,6 +28,7 @@ public class Controller {
 
         seconds = 0;
         setSecondsText();
+        saveData();
     }
 
     public void onMouseDecrease(){
@@ -34,11 +36,13 @@ public class Controller {
             Main.counter--;
             encounter.setText(Main.counter + "");
         }
+        saveData();
     }
 
     public void onMouseReset(){
         Main.counter = 0;
         encounter.setText(Main.counter + "");
+        saveData();
     }
 
     public void show(){
@@ -47,6 +51,11 @@ public class Controller {
 
     @FXML
     public void initialize() {
+        checkForDirectories();
+        checkForFile();
+
+        setEncounterNumber();
+
         t.schedule(new TimerTask() {
             public void run() {
                 Platform.runLater(new Runnable() {
@@ -61,6 +70,65 @@ public class Controller {
 
     void setSecondsText(){
         secondsLabel.setText(seconds + "");
+    }
+
+
+    private void checkForDirectories(){
+        File dataDir = new File("encounterData");
+        if (!dataDir.exists()) {
+            if (dataDir.mkdirs()) {
+                System.out.println("Directory is created!");
+            } else {
+                System.out.println("Failed to create directory!");
+            }
+        }
+    }
+
+    private boolean checkForFile(){
+        File file = new File("encounterData\\data.txt");
+
+        try {
+            if (file.createNewFile()){
+                saveData();
+                return true;
+            }else{
+                return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void saveData(){
+        File file = new File("encounterData\\data.txt");
+        try {
+            FileWriter writer = new FileWriter(file, false);
+            writer.write(Main.counter + "");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void setEncounterNumber(){
+        if(checkForFile()){
+            FileInputStream fis = null;
+
+            try {
+                fis = new FileInputStream("encounterData\\data.txt");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            assert fis != null;
+            Scanner scanner = new Scanner(fis);
+            String firstLine = scanner.nextLine();
+
+            Main.counter = Integer.parseInt(firstLine);
+            encounter.setText(Main.counter + "");
+        }
     }
 
 
